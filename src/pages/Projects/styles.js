0c064c2e97e93cff33d6c14b1e771e7c7da6ccf5
naeemdadi <naeemdadi@users.noreply.github.com>
2021-05-22 14:25:18 +0530

    Create a React app and publish it to GitHub Pages

---
+++ src/pages/Projects/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+import { white } from '@carbon/colors'
+
+export const ProjectItem = styled.li`
+  margin-top: 1rem;
+  padding-bottom: 1rem;
+  border-bottom: 1px solid ${white};
+`
+
+export const ProjectTitle = styled.h4`
+  font-weight: bold;
+`
+
+export const SkillContainer = styled.div`
+  margin-top: 1.2rem;
+`
+
+export const ProjectLinks = styled.div`
+  margin-top: 1.2rem;
+  font-weight: bold;
+  display: flex;
+  justify-content: space-between;
+`
