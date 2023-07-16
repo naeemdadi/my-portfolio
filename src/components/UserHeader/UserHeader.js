be447c1b09eb22d00bb3e374fc9067eaef2ce863
Naeem Dadi <63260506+naeemdadi@users.noreply.github.com>
2023-07-16 23:54:49 +0530

    Update Resume

--- src/components/UserHeader/UserHeader.js
+++ src/components/UserHeader/UserHeader.js
@@ -51,7 +51,7 @@ const UserHeader = ({ user }) => {
       <div>
         <ViewResumeLink
           // href={`https://gitconnected.com/${user.basics.username}/resume`}
-          href="https://drive.google.com/file/d/1ShFHkQyQAq4AAFE-9Yn2JSXVsB1F2wkN/view?usp=sharing"
+          href="https://drive.google.com/file/d/1vO_BkFIrkt1UQoRBiYLf6pvkdIdXOij5/view?usp=drivesdk"
           target="_blank"
           rel="noopener noreferrer"
         >
